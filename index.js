0abcc3715eec220cb02d67f9f633f9e08f8db07c
Kartikey Khanna <135053308+SnappyKODE@users.noreply.github.com>
2023-09-19 21:28:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let nav_items = $(".navbar a");
 let effect_hover = $(".blur");
+let bar = $(".bar");
+let c=0;
 nav_items.click(function(){
     $(this).addClass("cc");
     nav_items.not(this).removeClass("cc");
+    bar.text("menu");
+    $(".navbar").addClass("hide_menu");
+    $("#home,#skill,#project").removeClass("shadow");
+    c=0;
 })
 
 effect_hover.mouseover(function(){
@@ -12,4 +18,20 @@ effect_hover.mouseover(function(){
 
 effect_hover.mouseleave(function(){
     $(this).addClass("hide");
+})
+
+bar.click(function(){
+    if(c==0){
+        bar.text("close");
+        $(".navbar").removeClass("hide_menu");
+        $("#home,#skill,#project").addClass("shadow");
+        c=1;
+    }
+    else{
+        bar.text("menu");
+        $(".navbar").addClass("hide_menu");
+        $("#home,#skill,#project").removeClass("shadow");
+        c=0;
+    }
+    
 })
